c2f0979858a8b8de465c8803bd34515ea6cdb2a5
Your Name <you@example.com>
2022-08-16 04:15:59 +0000

    added new data source on energy

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -8,19 +8,17 @@ import pandas as pd
 
 ########### Define your variables ######
 
-tabtitle = 'Old McDonald'
-sourceurl = 'https://plot.ly/python/choropleth-maps/'
-githublink = 'https://github.com/austinlasseter/agriculture-exports-map'
+tabtitle = 'Energy Stats by US States - 2014'
+sourceurl = 'https://www.kaggle.com/datasets/lislejoem/us_energy_census_gdp_10-14'
+githublink = 'https://github.com/xshakib/306-agriculture-exports-dropdown'
 # here's the list of possible columns to choose from.
-list_of_columns =['total exports', 'beef', 'pork', 'poultry',
-       'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
-       'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
+list_of_columns =['TotalC2014', 'BiomassC2014', 'CoalC2014', 'ElecC2014', 'FossilFuelC2014', 'GeoC2014', 'HydroC2014']
 
 
 ########## Set up the chart
 
 import pandas as pd
-df = pd.read_csv('assets/usa-2011-agriculture.csv')
+df = pd.read_csv('assets/Energy_data.csv')
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -31,14 +29,14 @@ app.title=tabtitle
 ########### Set up the layout
 
 app.layout = html.Div(children=[
-    html.H1('2011 Agricultural Exports, by State'),
+    html.H1('2014 Energy Consumption Stats by State'),
     html.Div([
         html.Div([
                 html.H6('Select a variable for analysis:'),
                 dcc.Dropdown(
                     id='options-drop',
                     options=[{'label': i, 'value': i} for i in list_of_columns],
-                    value='corn'
+                    value='TotalC2014'
                 ),
         ], className='two columns'),
         html.Div([dcc.Graph(id='figure-cancan'),
@@ -55,12 +53,12 @@ app.layout = html.Div(children=[
 @app.callback(Output('figure-1', 'figure'),
              [Input('options-drop', 'value')])
 def make_figure(varname):
-    mygraphtitle = f'Exports of {varname} in 2011'
+    mygraphtitle = f'Consumption of {varname} in 2014'
     mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
-    mycolorbartitle = "Millions USD"
+    mycolorbartitle = "Billion BTUs"
 
     data=go.Choropleth(
-        locations=df['code'], # Spatial coordinates
+        locations=df['StateCodes'], # Spatial coordinates
         locationmode = 'USA-states', # set of locations match entries in `locations`
         z = df[varname].astype(float), # Data to be color-coded
         colorscale = mycolorscale,
